905a4f4cdae68199994b37e6d3691d3c93720d5f
Prasu7432 <137610262+Prasu7432@users.noreply.github.com>
2023-12-14 23:27:53 +0530

    Update for Beginners.py

--- for Beginners.py
+++ for Beginners.py
@@ -1,11 +1,37 @@
-print("This Program is to calculate the BMI of an individual\n")
-name = input("Enter Your Name : ")
-weight = float(input("Enter Your Weight : "))
-height = float(input("Enter Your Height : "))
+import random
 
-bmi = weight / (height ** 2)
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-if bmi < 25:
-    print(f"{name} is underweight by {bmi} BMI")
-else:
-    print(f"{name} is overweight by {bmi} BMI")   
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(numbers))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+print("char", char)
+
+# convert list to string
+pwd = ''.join(password_list)
+print(f"Your random password to use is: {pwd}")
